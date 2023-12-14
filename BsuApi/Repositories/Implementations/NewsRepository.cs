555f0443e2ffe039bff31473b5e9086d788ca661
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-12-14 18:46:16 +0300

    added implementation for news

---
+++ BsuApi/Repositories/Implementations/NewsRepository.cs
@@ -0,0 +1,14 @@
+﻿using DbConect;
+using Model;
+using Repositories.Implimentations;
+
+namespace BsuApi.Repositories.Implementations
+{
+    public class NewsRepository : BaseRepository<News>
+    {
+        public NewsRepository(MyDbContext context) : base(context)
+        {
+        }
+
+    }
+}
