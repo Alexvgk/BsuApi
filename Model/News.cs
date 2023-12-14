65f4041e9700bd5a0c9e337ced35d53b12d32d69
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-12-14 03:02:36 +0300

    added news entity

---
+++ Model/News.cs
@@ -0,0 +1,16 @@
+﻿using Model.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class News : BaseModel
+    {
+        public string? Text { get; set; }
+        public byte[]? ImageData { get; set; }
+        public virtual CourseGroup? CourseGroup { get; set; } 
+    }
+}
